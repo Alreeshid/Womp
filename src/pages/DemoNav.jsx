62aae631423085e635f18a6be8d66865536c0083
Big Burger <110642010+Alreeshid@users.noreply.github.com>
2025-02-17 15:27:57 -0500

    make demo time?

---
+++ src/pages/DemoNav.jsx
@@ -0,0 +1,25 @@
+import { useState, useEffect } from "react";
+import { Button, Flex, View } from '@aws-amplify/ui-react';
+
+
+export default function Landing(){
+    return (
+        <View
+          height="100vh"
+          width="100%"
+          backgroundColor="white"
+        >
+          <Flex
+            direction="row"
+            justifyContent="center"
+            alignItems="center"
+            height="100%"
+            gap="medium"
+          >
+            <Button variation="primary">Login/Signup</Button>
+            <Button variation="primary">Ecommerce Landing Template</Button>
+            <Button variation="primary">Admin Landing Template</Button>
+          </Flex>
+        </View>
+      );
+    };
