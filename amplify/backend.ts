9939b26913d30ddce8c9ce045804dfea5f7ac35a
Big Burger <110642010+Alreeshid@users.noreply.github.com>
2025-03-16 16:34:33 -0400

    attempting to define new IAM policy to see if the query will now run

--- amplify/backend.ts
+++ amplify/backend.ts
@@ -4,12 +4,26 @@ import { data } from './data/resource';
 import { storage } from './storage/resource';
 import { listUsers } from './functions/list-users/resource';
 
+import * as iam from "aws-cdk-lib/aws-iam"
+
 /**
  * @see https://docs.amplify.aws/react/build-a-backend/ to add storage, functions, and more
  */
-defineBackend({
+const backend = defineBackend({
   auth,
   data,
   storage,
-  listUsers
+  listUsers,
 });
+
+const lambdaFunction = backend.listUsers.resources.lambda;
+lambdaFunction.role?.attachInlinePolicy(
+  new iam.Policy(backend.auth.resources.userPool, "AllowListUsers", {
+    statements: [
+      new iam.PolicyStatement({
+        actions: ["cognito-idp:ListUsers"],
+        resources: [backend.auth.resources.userPool.userPoolArn],
+      }),
+    ],
+  })
+);
